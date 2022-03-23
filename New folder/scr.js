d4f47faa1b13662fc2c2410a71b54be171e8e08e
Satyam5665 <96643434+Satyam5665@users.noreply.github.com>
2022-03-22 22:48:30 -0700

    Add files via upload

---
+++ New folder/scr.js
@@ -0,0 +1,63 @@
+// 
+function satyam(){
+    console.log("Hello satyam")
+}
+
+satyam()
+
+// let count =0
+// console.log(count)
+
+// let myage=19
+// console.log(myage)
+
+// let age=10
+// let dogratio=1.2
+
+// let dogage=age*dogratio
+
+// console.log(dogage)
+
+// let bonuspoint=100
+// bonuspoint=25
+// bonuspoint=70
+
+let name="Satyam"
+let greeting ="My Name is "
+
+let g=greeting+name
+
+console.log(g)
+
+let username="Satyam"
+let message="You have three new notifications"
+
+console.log("Helllo "+"vagvshvs"+message+username)
+
+let count=0
+
+let ct=document.getElementById("co")
+
+
+function increment(){
+    
+    count+=1
+    ct.innerText=count
+    
+}
+document.getElementById("co").innerText=count
+
+let cmt=document.getElementById("saves")
+
+function save(){
+    
+    cmt.innerText += " " + count
+  
+    count=0
+    ct.innerText=count
+    
+    
+}
+
+
+//Practice 
